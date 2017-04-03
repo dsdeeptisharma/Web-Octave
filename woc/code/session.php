5ab704422750b99130b0b61894a59b1ba19aca7d
Deepti Sharma <sharmadeepti888@gmail.com>
2017-04-03 22:43:21 +0530

    Update session.php

--- woc/code/session.php
+++ woc/code/session.php
@@ -2,14 +2,8 @@
 /*********************************************************************
 *
 * This file is a part of Web Interface to Octave project.
-* Copyright (C) 2008 Kolo Naukowe Numerykow Uniwersytetu Warszawskiego
-* (Students' Numerical Scientific Group of University of Warsaw)
-*
-* e-mail:knn@students.mimuw.edu.pl
-*
-* Distributed under terms of GPL License
-*
-*
+
+
 *********************************************************************/
 
 /*
