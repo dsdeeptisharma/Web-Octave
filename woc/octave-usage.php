196bdde8502867aa4d89eaeefd826e8267777790
Deepti Sharma <sharmadeepti888@gmail.com>
2017-04-03 22:49:58 +0530

    Update octave-usage.php

--- woc/octave-usage.php
+++ woc/octave-usage.php
@@ -2,14 +2,7 @@
 /*********************************************************************
 *
 * This file is a part of Web Interface to Octave project.
-* Copyright (C) 2008 Kolo Naukowe Numerykow Uniwersytetu Warszawskiego
-* (Students' Numerical Scientific Group of University of Warsaw)
-*
-* e-mail:knn@students.mimuw.edu.pl
-*
-* Distributed under terms of GPL License
-*
-*
+
 *********************************************************************/
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
@@ -17,8 +10,8 @@
 <html>
 <head>
    <title>Web interface to Octave - logfile</title>
-<meta NAME="description" CONTENT="Ko³o naukowe numeryków">
-<meta NAME="author" CONTENT="Ko³o naukowe numeryków">
+<meta NAME="description" CONTENT="KoÂ³o naukowe numerykÃ³w">
+<meta NAME="author" CONTENT="KoÂ³o naukowe numerykÃ³w">
 <meta NAME="keywords" CONTENT="student, obliczenia naukowe,
 Octave, PETSc, MPI, matematyka obliczeniowa, metody numeryczne, 
 analiza numeryczna">
