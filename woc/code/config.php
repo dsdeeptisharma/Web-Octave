ffb46e02da153e870b6f1d00b9608c56be5f199e
Deepti Sharma <sharmadeepti888@gmail.com>
2017-04-03 22:39:55 +0530

    Update config.php

--- woc/code/config.php
+++ woc/code/config.php
@@ -2,14 +2,7 @@
 /*********************************************************************
 *
 * This file is a part of Web Interface to Octave project.
-* Copyright (C) 2008 Kolo Naukowe Numerykow Uniwersytetu Warszawskiego
-* (Students' Numerical Scientific Group of University of Warsaw)
-*
-* e-mail:knn@students.mimuw.edu.pl
-*
-* Distributed under terms of GPL License
-*
-*
+
 *********************************************************************/
 
 /// Implements reading data from configuration files
